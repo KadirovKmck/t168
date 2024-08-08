462c1c0b4825b1e993027b95a026fb68b85ef643
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2024-08-08 19:16:50 +0600

    ss

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:t168/src/presentation/welcome%20view/questionnaire_screen/questionnaire_screen.dart';
+import 'package:t168/src/presentation/welcome%20view/splash_screen.dart';
 
 class T168 extends StatelessWidget {
   const T168({super.key});
@@ -10,7 +10,7 @@ class T168 extends StatelessWidget {
   Widget build(BuildContext context) {
     return ResponsiveSizer(
       builder: (p0, p1, p2) => const MaterialApp(
-        home: QuestionnaireScreen(),
+        home: SplashScreen(),
         debugShowCheckedModeBanner: false,
       ),
     );

8be73752faa4df335ed97887e532279debf333b9
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2024-08-10 23:07:39 +0600

    xdhfcghvjbm,n

--- lib/src/presentation/welcome view/questionnaire_screen/questionnaire_screen.dart
+++ lib/src/presentation/welcome view/questionnaire_screen/questionnaire_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:t168/src/core/components/costom_container_color.dart';
 import 'package:t168/src/presentation/welcome%20view/loding_screen/loding_screen.dart';
 import 'package:t168/src/presentation/welcome%20view/questionnaire_screen/constants/quasions_text.dart';
@@ -37,87 +38,106 @@ class _QuestionnaireScreenState extends State<QuestionnaireScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-    body:  Container(
-      decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/'))),
-      child: ContainerColor(
-          child: Stack(
-            children: [
-              AnimatedContainer(
-                duration: const Duration(seconds: 5),
-                curve: Curves.easeInOut,
-                color: Colors.black.withOpacity(0.7),
-                child: PageView.builder(
-                  controller: _pageController,
-                  itemCount: QuestionsText.questionPartsA.length,
-                  onPageChanged: (index) {
-                    setState(() {
-                      currentPageIndex = index;
-                      selectedIndex = -1;
-                    });
-                  },
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(3.0),
-                            child: Text.rich(
-                              TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: QuestionsText.questionPartsA[index],
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w400,
+      body: ContainerColor(
+        child: Stack(
+          children: [
+            AnimatedContainer(
+              duration: const Duration(seconds: 5),
+              curve: Curves.easeInOut,
+              color: Colors.black.withOpacity(0.7),
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: QuestionsText.questionPartsA.length,
+                onPageChanged: (index) {
+                  setState(() {
+                    currentPageIndex = index;
+                    selectedIndex = -1;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return Builder(builder: (context) {
+                    return DecoratedBox(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/a${index + 1}.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: Text.rich(
+                                TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: QuestionsText.questionPartsA[index],
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w400,
+                                      ),
                                     ),
-                                  ),
-                                  TextSpan(
-                                    text: QuestionsText.questionPartsB[index],
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w900,
+                                    TextSpan(
+                                      text: QuestionsText.questionPartsB[index],
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w900,
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
+                                textAlign: TextAlign.center,
                               ),
-                              textAlign: TextAlign.center,
                             ),
-                          ),
-                          SizedBox(height: 5.h),
-                          FrostedGlassButton(
-                            text: QuestionsText.answersA[index],
-                            onTap: () => onSelect(0),
-                          ),
-                          SizedBox(height: 1.h),
-                          FrostedGlassButton(
-                            text: QuestionsText.answersB[index],
-                            onTap: () => onSelect(1),
-                          ),
-                          SizedBox(height: 1.h),
-                          FrostedGlassButton(
-                            text: QuestionsText.answersC[index],
-                            onTap: () => onSelect(2),
-                          ),
-                          SizedBox(height: 1.h),
-                          FrostedGlassButton(
-                            text: QuestionsText.answersD[index],
-                            onTap: () => onSelect(3),
-                          ),
-                        ],
+                            SizedBox(height: 5.h),
+                            FrostedGlassButton(
+                              text: QuestionsText.answersA[index],
+                              onTap: () => onSelect(0),
+                            ),
+                            SizedBox(height: 1.h),
+                            FrostedGlassButton(
+                              text: QuestionsText.answersB[index],
+                              onTap: () => onSelect(1),
+                            ),
+                            SizedBox(height: 1.h),
+                            FrostedGlassButton(
+                              text: QuestionsText.answersC[index],
+                              onTap: () => onSelect(2),
+                            ),
+                            SizedBox(height: 1.h),
+                            FrostedGlassButton(
+                              text: QuestionsText.answersD[index],
+                              onTap: () => onSelect(3),
+                            ),
+                            SizedBox(height: 5.h),
+                            SmoothPageIndicator(
+                              controller: _pageController,
+                              count: QuestionsText.questionPartsA.length,
+                              effect: ExpandingDotsEffect(
+                                expansionFactor: 2.0,
+                                dotWidth: 9.0,
+                                dotHeight: 9.0,
+                                spacing: 4.0,
+                                dotColor: Colors.grey.shade300,
+                                activeDotColor: Colors.red.shade700,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
-                  },
-                ),
+                  });
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-    ),
+      ),
     );
   }
 }
